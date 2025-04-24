0c43cb6723f587bcf81472f20206420f661f06a1
Baiyangx <50959808+Baiyangx@users.noreply.github.com>
2025-04-24 16:09:02 +0000

    Deploying to gh-pages from @ Baiyangx/Baiyangx.github.io@c68e267960eb6b19181130ce0617324fbbb21f64 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-neurips-2020",
+    title: "NeurIPS 2020",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
